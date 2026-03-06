2b66d8ca43a219c0213c53cfb02d350bbef4a53e
jonatha23456789 <159666966+jonatha23456789@users.noreply.github.com>
2026-03-06 04:25:55 +0100

    Add files via upload

---
+++ commands/Imagine(1).js
@@ -0,0 +1,68 @@
+const axios = require("axios");
+const FormData = require("form-data");
+
+module.exports = {
+  name: "imagine",
+  description: "Generate image from prompt",
+  usage: "-imagine <prompt>",
+  author: "Jonathan",
+
+  async execute(senderId, args, pageAccessToken) {
+    try {
+
+      const prompt = args.join(" ");
+      if (!prompt) {
+        return;
+      }
+
+      const api =
+        `https://christus-api.vercel.app/image/animagine?prompt=${encodeURIComponent(prompt)}`;
+
+      const { data } = await axios.get(api);
+
+      if (!data.status || !data.image_url) {
+        throw new Error("API failed");
+      }
+
+      const imageUrl = data.image_url;
+
+      // download image
+      const img = await axios.get(imageUrl, {
+        responseType: "arraybuffer"
+      });
+
+      const form = new FormData();
+
+      form.append(
+        "recipient",
+        JSON.stringify({
+          id: senderId
+        })
+      );
+
+      form.append(
+        "message",
+        JSON.stringify({
+          attachment: {
+            type: "image",
+            payload: {}
+          }
+        })
+      );
+
+      form.append("filedata", Buffer.from(img.data), "ai.png");
+
+      await axios.post(
+        `https://graph.facebook.com/v19.0/me/messages?access_token=${pageAccessToken}`,
+        form,
+        { headers: form.getHeaders() }
+      );
+
+    } catch (err) {
+      console.error(
+        "Imagine CMD Error:",
+        err.response?.data || err.message
+      );
+    }
+  }
+};
